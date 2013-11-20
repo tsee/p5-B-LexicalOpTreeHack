7aa3358a905a1ff0dd4c46d8c7aac6d93f9ab8b7
Steffen Mueller <smueller@cpan.org>
2013-11-20 18:53:51 +0100

    Initial checkin

---
+++ lib/B/LexicalOpTreeHack.pm
@@ -0,0 +1,12 @@
+package B::LexicalOpTreeHack;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+# ABSTRACT: frobnicates foobles
+
+require XSLoader;
+XSLoader::load("B::LexicalOpTreeHack", $VERSION);
+
+1;
+__END__
+
